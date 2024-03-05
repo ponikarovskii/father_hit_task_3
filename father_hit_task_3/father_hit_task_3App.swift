575b98002597cb72ce3f2a9e8dcfda3309b0e641
ponikarovskii <ponikarovskii.anton@gmail.com>
2024-03-05 22:58:47 +0100

    Initial Commit

---
+++ father_hit_task_3/father_hit_task_3App.swift
@@ -0,0 +1,17 @@
+//
+//  father_hit_task_3App.swift
+//  father_hit_task_3
+//
+//  Created by Антон Поникаровский on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct father_hit_task_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
